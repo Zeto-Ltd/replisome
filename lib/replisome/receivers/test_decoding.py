7fbf2bcdf4a90e68717fb2cc46bb75dfc2a5af84
Shane Breatnach <shane@zeto.ie>
2018-04-03 18:06:37 +0100

    Attempt at fixing WAL backlog with LSN introspection.

--- lib/replisome/receivers/test_decoding.py
+++ lib/replisome/receivers/test_decoding.py
@@ -5,6 +5,17 @@ __all__ = ['TestDecodingReceiver']
 
 class TestDecodingReceiver(BaseReceiver):
 
+    @classmethod
+    def from_config(cls, config):
+        opts = []
+        for k in ('include-xids', 'include-timestamp', 'skip-empty-xacts',
+                  'only-local', 'include-rewrites', 'force-binary'):
+            if k in config:
+                v = config.pop(k)
+                opts.append((k, (v and 't' or 'f')))
+
+        return cls(options=opts)
+
     def process_payload(self, raw_payload):
         payload = raw_payload.decode('utf-8')
         self.logger.debug(
